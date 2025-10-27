a1393b8c4c24feede0bbd9d51e239de7d046b53d
Elizabeth Bey <ebey04@gmail.com>
2025-10-27 12:14:16 -0400

    feat: add max limit to prompt()

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ const btn = document.querySelector('.btn');
 function showPrompt() {
     const userInput = prompt("How many boxes would you like?");
 
-if (userInput !== null) {
-  const num = Number(userInput);
-  section.innerHTML = "";
-  gridSize(num);
+    if (userInput > 100) {
+        alert("Please choose 100 or fewer squares.")
+        return;
+    }
+    if (userInput !== null) {
+        const num = Number(userInput);
+        section.innerHTML = "";
+    gridSize(num);
 } else {
-  document.getElementById("output").textContent = `You clicked Cancel.`;
+    document.getElementById("output").textContent = `You clicked Cancel.`;
 }
 }
 
@@ -21,15 +25,15 @@ function gridSize(n) {
     const containerWidth = section.clientWidth;
     const containerHeight = section.clientHeight;
 
-  for (let i = 0; i < size; i++) { 
+    for (let i = 0; i < size; i++) { 
     const div = document.createElement('div'); 
     div.classList.add('box'); 
 div.style.width = `${containerWidth / n}px`;
 div.style.height = `${containerHeight / n}px`;
 
     div.addEventListener("mouseover", () => {
-      div.classList.add('hover');
+    div.classList.add('hover');
     });
     section.appendChild(div);
-  }
+    }
 }
